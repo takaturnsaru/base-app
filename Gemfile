3deb827c2ec0220bfbe44bb0c87fd90f3c53535d
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2020-09-28 14:24:11 +0900

    ログイン前トップページフロント実装

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ group :production do
 end
 
 gem "simple_calendar","~>2.0"
+
+gem "devise"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'schedules#index'
   resources :schedules, only: [:index] do
     resources :meetings 
