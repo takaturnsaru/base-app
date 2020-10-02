373deef6cae1bc94148531360d3f481078712897
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2020-10-02 18:20:17 +0900

    テーブルの設計

---
+++ db/migrate/20201002090715_create_work_times.rb
@@ -0,0 +1,9 @@
+class CreateWorkTimes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :work_times do |t|
+      t.references :user,          null:false,foregin_key:true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_075337) do
+ActiveRecord::Schema.define(version: 2020_10_02_090715) do
 
   create_table "clients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -71,6 +71,13 @@ ActiveRecord::Schema.define(version: 2020_10_01_075337) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "work_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_work_times_on_user_id"
   end
 
 end
