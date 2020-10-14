651d797a57563e951327c5737013d03cb4ddd0b1
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2020-10-14 21:34:15 +0900

    管理者機能実行

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :products
+
+         validates :name,presence:true
 end
