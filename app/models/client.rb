7b9083a9d3b698e2dd23e778ed7846ef988ef615
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2020-10-29 16:53:49 +0900

    エラー文削除

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,10 +8,10 @@ class Client < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :postal_code,format: {with:/\A\d{3}[-]\d{4}\z/,message:"Postal code Input correctly"}
-    validates :prefectures_id, numericality: { other_then: 0, message:"Prefecture Select"}
+    validates :postal_code,format: {with:/\A\d{3}[-]\d{4}\z/}
+    validates :prefectures_id, numericality: { other_then: 0, message:"都道府県を選択してください"}
     validates :city
     validates :address
-    validates :phone_number, length:{maximum: 11}, format: {with:/\A\d{10,11}\z/,message:"Phone number can't be blank."}
+    validates :phone_number, length:{maximum: 11}, format: {with:/\A\d{10,11}\z/}
   end
 end
