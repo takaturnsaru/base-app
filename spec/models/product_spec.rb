0a9903790bd2fa7560874fa95702c74707d9be5e
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2020-11-01 17:26:46 +0900

    productモデル単体テスト

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Product, type: :model do
+  before do
+    @product = FactoryBot.build(:product)
+  end
+  
+  describe "製品の新規登録ができるとき" do
+    context "新規登録がうまくいくとき" do
+      it "nameが存在すれば登録できる" do
+        @product.name = "iii"
+        expect(@product).to be_valid
+      end
+      it "grade_idが0でなければ登録できる" do
+        @product.grade_id = 2
+        expect(@product).to be_valid
+      end
+      it "priceが存在すれば登録できる" do
+        @product.price = "2000000"
+        expect(@product).to be_valid
+      end
+    end
+
+    context "新規登録がうまくいかないとき" do
+      it "nameが空の場合登録できない" do
+        @product.name = ""
+        @product.valid?
+        expect(@product.errors.full_messages).to include("名前が入力されていません。")
+      end
+      it "grade_idが0の場合登録できない" do
+        @product.grade_id = 0
+        @product.valid?
+        expect(@product.errors.full_messages).to include("仕様を選択してください。")
+      end
+      it "priceがからの場合登録できない" do
+        @product.price = ""
+        @product.valid?
+        expect(@product.errors.full_messages).to include("価格が入力されていません。")
+      end
+    end
+  end
 end
